81b82e47ad7fa15e0581056f626187e7a06b643e
Soumyajeet2002 <sinkus14@gmail.com>
2026-02-26 19:04:17 +0530

    26-02-2026: User, Admin added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Role } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('register')
-  register(@Body() body: { email: string; password: string }) {
-    return this.usersService.create(body.email, body.password);
+  register(@Body() body: { email: string; password: string; role?: Role }) {
+    return this.usersService.create(
+      body.email,
+      body.password,
+      body.role ?? Role.USER,
+    );
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { User, Role } from './user.entity';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -11,13 +11,15 @@ export class UsersService {
     private userRepository: Repository<User>,
   ) {}
 
-  async create(email: string, password: string) {
+  async create(email: string, password: string, role: Role = Role.USER) {
+    // adding a role called user / by default
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = this.userRepository.create({
       email,
       password: hashedPassword,
       realpassword: password,
+      role,
       //this stores the plan password and the should not be done in this manner its not secure
     });
 
